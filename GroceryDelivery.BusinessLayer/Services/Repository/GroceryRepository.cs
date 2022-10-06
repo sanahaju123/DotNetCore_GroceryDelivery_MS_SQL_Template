f5c1ef3ca87e8c4cc89ad4501eb921ce92c26fba
sanahaju <sanahaju123@gmail.com>
2022-10-06 15:53:32 +0530

    Add project files.

---
+++ GroceryDelivery.BusinessLayer/Services/Repository/GroceryRepository.cs
@@ -0,0 +1,96 @@
+﻿using GroceryDelivery.DataLayer;
+using GroceryDelivery.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryDelivery.BusinessLayer.Services.Repository
+{
+    public class GroceryRepository : IGroceryRepository
+    {
+        /// <summary>
+        /// Creating Referance variable of GroceryDbContext
+        /// Injecting in GroceryRepository constructor
+        /// </summary>
+        private readonly GroceryDbContext _groceryContext;
+
+        public GroceryRepository(GroceryDbContext groceryDbContext)
+        {
+            _groceryContext = groceryDbContext;
+        }
+        /// <summary>
+        /// Add new product in InMemoryDb
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public async Task<Product> AddProduct(Product product)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get All product and product by id also
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> GetAllProduct(int ?id)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get product by Id
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <returns></returns>
+        public async Task<Product> GetProductById(int ProductId)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Menu List of catogery
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IList<Menubar>> MenuList()
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Place order
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public async Task<ApplicationUser> PlaceOrder(int ProductId, ApplicationUser user)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Find Product by Name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> ProductByName(string name)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get Order Information of ordered product
+        /// </summary>
+        /// <param name="UserId"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<ProductOrder>> OrderByuserId(int UserId)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GroceryDelivery/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GroceryDelivery.BusinessLayer.Interfaces;
+using GroceryDelivery.Entites;
+using GroceryDelivery.BusinessLayer.ViewModels;
+
+namespace Grocerydelivery.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        /// <summary>
+        /// Creating Referance variable of IGroceryServices
+        /// </summary>
+        private readonly IGroceryServices _groceryServices;
+        /// <summary>
+        /// Injecting the IGroceryServices in controller constructor
+        /// </summary>
+        /// <param name="groceryServices"></param>
+        public HomeController(IGroceryServices groceryServices)
+        {
+            _groceryServices = groceryServices;
+        }
+
+
+        /// <summary>
+        /// Show all products
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("get-all-products")]
+        public async Task<IActionResult> AllProducts()
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Show list of categories
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("list-all-categories")]
+        public async Task<IActionResult> AllCategory()
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Show the product Details by product id
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("product-details")]
+        public async Task<IActionResult> Details(int ProductId)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+
+        
+        /// <summary>
+        /// Place order by product id
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("place-order")]
+        public async Task<IActionResult> Placeorder(int ProductId, ApplicationUser user)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Used for show order info
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("order-info")]
+        public async Task<IActionResult> OrderInfo(int userId)
+        {
+            //do code here
+            throw new NotImplementedException();
+        }
+    }
+}
